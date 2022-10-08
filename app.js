da12b80402b6de1b03c0ffc8f0de3b80e43b9284
FedeSotoo <fedesoto28@gmail.com>
2022-10-08 06:18:02 -0300

    subo proyecto Mercado-Liebre terminado

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app =express();
+const path = require ('path');
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.resolve('public')))
+
+app.get('/',(req,res)=>
+res.sendFile(path.resolve(__dirname,'views/home.html')))
+
+app.get('/register',(req,res)=>
+res.sendFile(path.resolve(__dirname,'views/register.html')))
+
+app.get('/login',(req,res)=>
+res.sendFile(path.resolve(__dirname,'views/login.html')))
+
+app.listen(PORT, () => {
+    console.log("listening on port http://localhost:" + PORT);
+});
